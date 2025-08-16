eaa730c58b8ed507b693133abbd0815076b25a93
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-08-16 19:00:17 +0530

    add some simple styles

--- obj/Debug/net8.0/LoanManagementSystemAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/LoanManagementSystemAssignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanManagementSystemAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e3cd1dd41e61e6d3ce13078d76b1b99a8c454b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddebeb1350210b135255e354accc2660844fd4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanManagementSystemAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanManagementSystemAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
